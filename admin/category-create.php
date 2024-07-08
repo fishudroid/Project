e54a578aa85df1da2638135b31779b84df872ded
fishudroid <duc0981173026@gmail.com>
2024-07-08 10:02:55 +0700

    zzzz

--- admin/category-create.php
+++ admin/category-create.php
@@ -1,79 +1,83 @@
 <?php include 'header.php';
-$error = ''; 
+$error = '';
 if (isset($_POST['name'])) {
-  $name = $_POST['name']; 
+  $name = $_POST['name'];
   $status = $_POST['status'];
-  
-  if ($name == '' ) { 
+
+  if ($name == '') {
     $error = 'Tên danh mục không được trống';
-  } 
-  $query = $conn->query("SELECT * FROM category WHERE name = '$name'"); 
-  if ($query->num_rows > 0) { 
+  }
+  $query = $conn->query("SELECT * FROM category WHERE name = '$name'");
+  if ($query->num_rows > 0) {
     $error = 'Tên danh mục đã được sử dụng';
-  } 
-  if (!$error) { 
-    $sql = "INSERT INTO category (name, status) VALUES ('$name', '$status')"; 
-    if ($conn->query($sql)) { 
-      header('location: category.php'); 
-    } else { 
+  }
+  if (!$error) {
+    $sql = "INSERT INTO category (name, status) VALUES ('$name', '$status')";
+    if ($conn->query($sql)) {
+      header('location: category.php');
+    } else {
       $error = 'Thêm mới không thành công, vui lòng thử lại';
     }
   }
 }
 ?>
-  <div class="content-wrapper">
-    <!-- Content Header (Page header) -->
-    <section class="content-header">
-      <h1>Thêm danh mục</h1>
+<div class="content-wrapper">
+  <!-- Content Header (Page header) -->
+  <section class="content-header">
+    <h1>Thêm danh mục</h1>
 
-    </section>
-    <!-- Main content -->
-    <section class="content">                           
-     
-      <div class="box">
-        <div class="box-body">
-            <?php if($error): ?>
-            <div class="alert alert-danger">
-                <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
-                <strong>Lỗi !!</strong> <?php echo $error; ?>
+  </section>
+  <!-- Main content -->
+  <section class="content">
+
+    <div class="box">
+      <div class="box-body">
+        <?php if ($error) : ?>
+          <div class="alert alert-danger">
+            <button type="button" class="close" data-dismiss="alert" aria-hidden="true">&times;</button>
+            <strong>Lỗi !!</strong> <?php echo $error; ?>
+          </div>
+        <?php endif; ?>
+
+        <form action="" method="POST" role="form">
+          <div class="row">
+            <div class="col-md-4">
+              <div class="panel panel-default">
+                <div class="panel-body">
+                  <div class="form-group">
+                    <label for="">Tên danh mục</label>
+                    <input type="text" class="form-control" name="name" placeholder="Điền tên danh mục muốn tạo">
+                  </div>
+
+                  <div class="form-group">
+                    <label for="">Tên danh mục</label>
+
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="status" value="1" checked=>
+                        Hiển thị
+                      </label>
+                    </div>
+                    <div class="radio">
+                      <label>
+                        <input type="radio" name="status" value="0" checked=>
+                        Tạm ẩn
+                      </label>
+                    </div>
+                  </div>
+                  <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Lưu lại</button>
+                  <a href="category.php" class="btn btn-warning"><i class="fa fa-arrow-left"></i>Quay lại</a>
+                </div>
+              </div>
             </div>
-           <?php endif; ?> 
-       
-       <form action="" method="POST" role="form">
-       
-        <div class="form-group">
-            <label for="">Tên danh mục</label>
-            <input type="text" class="form-control" name="name" placeholder="Điền tên danh mục muốn tạo">
-        </div>
-         
-        <div class="form-group">
-            <label for="">Tên danh mục</label>
-            
-            <div class="radio">
-                <label>
-                    <input type="radio" name="status" value="1" checked=>
-                  Hiển thị
-                </label>
-            </div>
-            <div class="radio">
-                <label>
-                    <input type="radio" name="status" value="0" checked=>
-                  Tạm ẩn
-                </label>
-            </div>
-        </div>
-       
-        
-       
-        <button type="submit" class="btn btn-primary"><i class="fa fa-save"></i> Lưu lại</button>
-        <a href="category.php" class="btn btn-warning"><i class="fa fa-arrow-left"></i>Quay lại</a>
-       </form>
-       
-        </div>
+          </div>
+        </form>
+
       </div>
-      <!-- /.box -->
-    </section>
+    </div>
+    <!-- /.box -->
+  </section>
 
-  </div>
+</div>
 
-  <?php include 'footer.php';?>
+<?php include 'footer.php'; ?>
